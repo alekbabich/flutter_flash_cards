4109bfb136c3ea626a054a5e917a493444e4a43a
Alek Babich <ababich@vividcloud.com>
2022-11-27 21:03:20 -0500

    Add flashcard class, prev and next button

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,82 @@
+import 'package:flash_cards/flashcard_view.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
+import 'flashcard.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const FlashCards());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlashCards extends StatefulWidget {
+  const FlashCards({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<FlashCards> createState() => _FlashCardsState();
+}
+
+class _FlashCardsState extends State<FlashCards> {
+  final List<Flashcard> _flashcards = [
+    Flashcard(question: "Is this question 1?", answer: "Yes"),
+    Flashcard(question: "Is this question 3?", answer: "No"),
+    Flashcard(question: "Is this question 3?", answer: "Yes"),
+    Flashcard(question: "Is this question 4?", answer: "Yes"),
+    Flashcard(question: "Is this question 5?", answer: "Yes"),
+    Flashcard(question: "Is this question 6?", answer: "Yes"),
+  ];
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: FlipCard(
+                    front: FlashcardView(
+                        text: _flashcards[_currentIndex].question),
+                    back:
+                        FlashcardView(text: _flashcards[_currentIndex].answer),
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton.icon(
+                    onPressed: showPreviousCard,
+                    icon: const Icon(Icons.chevron_left),
+                    label: const Text('Prev'),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: showNextCard,
+                    icon: const Icon(Icons.chevron_right),
+                    label: const Text('Next'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void showNextCard() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex =
+          _currentIndex + 1 < _flashcards.length ? _currentIndex + 1 : 0;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void showPreviousCard() {
+    setState(() {
+      _currentIndex =
+          _currentIndex >= 1 ? _currentIndex - 1 : _flashcards.length - 1;
+    });
   }
 }
